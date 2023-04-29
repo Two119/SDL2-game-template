9167cd073023ef80e1030339cb803e3871948313
Two119 <mangaartisttwo@gmail.com>
2023-04-29 13:39:34 +0530

    Added required dlls and fixed errors

---
+++ run.py
@@ -0,0 +1,3 @@
+import subprocess
+subprocess.run("g++ -o test.exe main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_Image -lSDL2_mixer -lSDL2_mixer.dll -lSDL2_ttf -lSDL2_ttf.dll -I src")
+subprocess.run("test.exe")
